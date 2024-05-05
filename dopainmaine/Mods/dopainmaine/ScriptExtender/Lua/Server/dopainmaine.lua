9179cf7ca16cd613afce50c7bd60b42cd705f493
Ken Bynell <ken.bynell@gmail.com>
2024-05-05 01:44:12 -0500

    massive update

--- dopainmaine/Mods/dopainmaine/ScriptExtender/Lua/Server/dopainmaine.lua
+++ dopainmaine/Mods/dopainmaine/ScriptExtender/Lua/Server/dopainmaine.lua
@@ -1,1 +1,12 @@
- 
+function OnStatsLoaded()
+    for _, resourceGuid in pairs(Ext.StaticData.GetAll("Race")) do
+        Ext.StaticData.Get(resourceGuid, "Race").ExcludedGods = {}
+		Ext.StaticData.Get(resourceGuid, "Race").Gods = {}
+    end
+
+    for _, resourceGuid in pairs(Ext.StaticData.GetAll("ClassDescription")) do
+        Ext.StaticData.Get(resourceGuid, "ClassDescription").HasGod = true
+    end
+end
+
+Ext.Events.StatsLoaded:Subscribe(OnStatsLoaded)
